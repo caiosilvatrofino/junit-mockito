a884a210e8243e659965ca0771844ed2efe264f6
caiosilvatrofino <caios.trofino@gmail.com>
2025-01-19 00:27:48 -0300

    test resource

--- demo/src/main/java/com/trofino/demo/domain/User.java
+++ demo/src/main/java/com/trofino/demo/domain/User.java
@@ -1,14 +1,12 @@
 package com.trofino.demo.domain;
 
 import jakarta.persistence.*;
-import lombok.*;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
+
 @Entity
 public class User {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -17,4 +15,43 @@ public class User {
     @Column(unique = true)
     private String email;
     private String password;
+
+    public User(Integer id, String name, String password, String email) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
