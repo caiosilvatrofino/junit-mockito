ed32616eccdb0298bbf6930cbbbc74f7f9166dea
caiosilvatrofino <caios.trofino@gmail.com>
2025-01-19 19:16:10 -0300

    endpoint getall

--- demo/src/main/java/com/trofino/demo/domain/dto/UserDTO.java
+++ demo/src/main/java/com/trofino/demo/domain/dto/UserDTO.java
@@ -2,13 +2,14 @@ package com.trofino.demo.domain.dto;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class UserDTO {
 
     private Integer id;
     private String name;
     private String email;
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     public UserDTO() {
